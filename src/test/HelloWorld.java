7942f08519f01421db1ec64078fa2ad7589c64e4
kende831208 <kende82108@163.com>
2018-09-01 20:28:16 +0800

    第二次提交：修改了HelloWorld里面的代码

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -3,5 +3,9 @@ package test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello, 这是是测试git！！！");
+		
+		System.out.println("hello, 这是是测试git2！！！");
+		
+		System.out.println("hello, 这是是测试git3！！！");
 	}
 }
